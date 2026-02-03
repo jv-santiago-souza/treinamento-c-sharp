b92d85d2db31e15f70d23e105e2418509e483023
João Vitor S. Souza <jv.santiago.souza@gmail.com>
2026-02-03 15:08:38 -0300

    Inicialização da estrutura Git e "ProjetoTreino" no Visual Studio

---
+++ RodrigoSendin/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo bonito!");
+        }
+    }
+}
