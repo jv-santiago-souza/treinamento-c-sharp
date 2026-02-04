382a4d4e6db13e8d8830b1b75180103d744c99db
João Vitor S. Souza <jv.santiago.souza@gmail.com>
2026-02-04 16:18:50 -0300

    datetime e timespan

--- RodrigoSendin/ExerciciosTDD/ExerciciosTDD.Tests/HelloWorldTest.cs
+++ RodrigoSendin/ExerciciosTDD/ExerciciosTDD.Tests/HelloWorldTest.cs
@@ -4,9 +4,11 @@ using ExerciciosTDD.Domain;
 namespace ExerciciosTDD.Tests
 {
     [TestClass]
-    public sealed class HelloWorldTest{
+    public sealed class HelloWorldTest
+    {
         [TestMethod]
-        public void SayHello_Test(){
+        public void SayHello_Test()
+        {
             string mensagem = HelloWorld.SayHello();
 
             Assert.AreEqual("Hello, World!", mensagem);
@@ -50,6 +52,107 @@ namespace ExerciciosTDD.Tests
         public void MinhaClasse_Test()
         {
             var obj = new MinhaClasse();
+        }
+
+        [TestMethod]
+        public void DateTime_Test()
+        {
+            var hoje = DateTime.Today;
+            Console.WriteLine(hoje);
+
+            var agora = DateTime.Now;
+            Console.WriteLine(agora);
+        }
+
+        [TestMethod]
+        public void DateTime_Desmembrando_Test()
+        {
+            var agora = DateTime.Now;
+
+            Console.WriteLine(agora);
+            Console.WriteLine(agora.Year);
+            Console.WriteLine(agora.Month);
+            Console.WriteLine(agora.Day);
+            Console.WriteLine(agora.Hour);
+            Console.WriteLine(agora.Minute);
+            Console.WriteLine(agora.Second);
+            Console.WriteLine(agora.Millisecond);
+            Console.WriteLine(agora.DayOfWeek);
+            Console.WriteLine(agora.DayOfYear);
+        }
+
+        [TestMethod]
+        public void DateTime_Add_Test()
+        {
+            var agora = DateTime.Now;
+            Console.WriteLine(agora);
+
+            var mais5horas = agora.AddHours(5);
+            Console.WriteLine(mais5horas);
+            Console.WriteLine(mais5horas.GetType());
+
+            var amanha = agora.AddDays(1);
+            Console.WriteLine(amanha);
+
+            var ontem = agora.AddDays(-1);
+            Console.WriteLine(ontem);
+        }
+
+        [TestMethod]
+        public void DateTime_Inicializacao_Test()
+        {
+            var data = new DateTime(2007, 06, 01); // Ano, Mês, Dia
+            Console.WriteLine(data);
+
+            var dataComHora = new DateTime(2007, 06, 01, 18, 33, 0);
+            Console.WriteLine(dataComHora);
+        }
+
+        [TestMethod]
+        public void DateTime_Convertendo_String_Test()
+        {
+            var data = DateTime.Parse("2024-06-15 14:30:00");
+        }
+
+        [TestMethod]
+        public void DateTime_Quantidade_Dias_No_Mes_Test()
+        {
+            var diasFev2024 = DateTime.DaysInMonth(2024, 2);
+            Console.WriteLine(diasFev2024);
+            var diasFev2023 = DateTime.DaysInMonth(2023, 2);
+            Console.WriteLine(diasFev2023);
+
+            var ultimoDiaFev2024 = new DateTime(2024, 2, diasFev2024);
+        }
+
+        [TestMethod]
+        public void DateTime_Formatacoes_Test()
+        {
+            var agora = DateTime.Now;
+            Console.WriteLine(agora);
+            Console.WriteLine(agora.ToString("d"));
+            Console.WriteLine(agora.ToString("G"));
+            Console.WriteLine(agora.ToString("f"));
+
+            Console.WriteLine(agora.ToString("dd/MM/yyyy"));
+            Console.WriteLine(agora.ToString("dd/MM/yyyy HH:mm"));
+
+            Console.WriteLine(agora.ToString("MMMM"));
+        }
+
+        [TestMethod]
+        public void TimeSpan_Test()
+        {
+            var hoje = DateTime.Now;
+            var amanha = hoje.AddDays(1);
+
+            var dif = amanha - hoje; // operador de subtração entre DateTimes resulta em TimeSpan
+            Console.WriteLine(dif);
+            var diferenca = amanha.Subtract(hoje); // método Subtract entre DateTimes resulta em TimeSpan (mesma coisa que o operador acima)
+            Console.WriteLine(diferenca);
+
+            Console.WriteLine(dif.TotalSeconds);
+            Console.WriteLine(dif.TotalHours);
         }
     }
 }
